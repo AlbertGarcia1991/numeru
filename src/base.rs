174b4424e24d5be59d4114eeb91005d2b5fa920e
Albert Garcia Plaza <gplaza91@gmail.com>
2024-03-12 09:28:37 +0000

    WIP Tensors, added constructors to create Tensors full of 1s and 0s

--- src/base.rs
+++ src/base.rs
@@ -1,26 +1,47 @@
 /// A N-dimensional tensor of generic data type
-pub struct Tensor<T> {
+#[derive(Debug, Clone)]
+pub struct Tensor<T>
+{
     /// A flat vector that contains all the elements of the tensor. Rust's Vec<T> is a resizable array type that provides safe and efficient access to elements.
-    data: Vec<T>,
+    pub data: Vec<T>,
     /// A vector of usize that represents the size of the tensor in each dimension. For a 2D tensor (matrix), the shape might be [rows, cols].
-    shape: Vec<usize>,
-    /// Number of dimensions, equal to the length of the shape vector.
-    dims: usize,
+    pub shape: Vec<usize>,
     /// Strides are used to calculate the index of an element in the flat data vector based on its multi-dimensional indices. This is crucial for efficiently accessing and manipulating tensor elements.
-    strides: Vec<usize>
+    pub strides: Vec<usize>,
+    /// Row-major length is the length of the flattened tensor
+    pub row_major_length: usize
 }
 
-impl<T> Tensor<T> {
-    /// CONSTRUCTORS
+impl<T> Tensor<T> where T: From<u8> + Copy
+{
+
+    // TODO: Use vec versus array
 
     pub fn new(data: Vec<T>, shape: Vec<usize>) -> Self {
         // TODO: Do we actually require to pass the shape, or can be read from input data? 
-        let strides = Self::compute_strides(&shape);
-        let dims: usize = shape.len();
-        return Tensor{ data, shape, dims, strides };
+        if data.len() != shape.iter().product() {
+            panic!("Data length does not match the product of the shape dimensions");
+        }
+        let strides: Vec<usize> = Self::compute_strides(&shape);
+        let row_major_length: usize = shape.iter().product();
+        Tensor{ data, shape, strides, row_major_length }
     }
 
-    // TODO: Create constructos to ones, zeros, fill, random, eye, as well as the *_like versions
+    pub fn ones(shape: Vec<usize>) -> Self {
+        let row_major_length: usize = shape.iter().product();
+        let data: Vec<T> = vec![T::from(1); row_major_length];
+        let strides: Vec<usize> = Self::compute_strides(&shape);
+        Tensor{ data, shape, strides, row_major_length }
+    }
+
+    pub fn zeros(shape: Vec<usize>) -> Self {
+        let row_major_length: usize = shape.iter().product();
+        let data: Vec<T> = vec![T::from(0); row_major_length];
+        let strides: Vec<usize> = Self::compute_strides(&shape);
+        Tensor{ data, shape, strides, row_major_length }
+    }
+
+    // TODO: Create constructors to eye, as well as the *_like versions
 
     /// Strides are pivotal in efficiency accessing elements in a multi-dimensional tensor when it is stored in a linear memory space.
     /// Strides represents the "step" needed to move along each dimension of the tensor. In the concept of the Tensor, strides is a vector
@@ -40,8 +61,8 @@ impl<T> Tensor<T> {
     /// to move to the next one in a column, we need to move 4 positions in the row-major order vector. That means that the strides for
     /// this tensor are [4, 1]
     fn compute_strides(shape: &Vec<usize>) -> Vec<usize> {
-        let mut strides = Vec::with_capacity(shape.len());
-        let mut stride = 1;
+        let mut strides: Vec<usize> = Vec::with_capacity(shape.len());
+        let mut stride: usize = 1;
         for &dimension in shape.iter().rev() {
             strides.push(stride);
             stride *= dimension;
@@ -54,7 +75,7 @@ impl<T> Tensor<T> {
     // If possible, add test about that
 
     fn get(&self, indices: &[usize]) -> Option<&T> {
-        let index = self.compute_flat_index(indices)?;
+        let index: usize = self.compute_flat_index(indices)?;
         return self.data.get(index);
     }
 
@@ -74,25 +95,49 @@ impl<T> Tensor<T> {
         return Some(flat_index);
     }
 
+    // OVERLOADING
     // TODO: Pretty-printing
+    // TODO: Aritmetic operations
+    // TODO: Comparisons
 }
+
+// TODO: Modulus and other metrics as a trait
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn get_dummy_tensor_from_new() -> Tensor<i32> {
+        let data: Vec<i32> = vec![1, 2, 3, 4, 5, 6];
+        let shape: Vec<usize> = vec![2, 3];
+        let tensor: Tensor<i32> = Tensor::new(data, shape);
+        tensor
+    }
+
     #[test]
     fn test_new_attributes() {
-
+        let tensor: Tensor<i32> = get_dummy_tensor_from_new();
+        assert_eq!(tensor.shape, vec![2, 3]);
+        assert_eq!(tensor.strides, vec![3, 1]);
+    }
+    
+    #[test]
+    fn test_ones() {        
+        let tensor: Tensor<i32> = Tensor::ones(vec![2, 2]);
+        assert_eq!(tensor.shape, vec![2, 2]);
+        assert_eq!(tensor.strides, vec![2, 1]);
+        for idx in 0..tensor.row_major_length {
+            assert_eq!(tensor.data[idx], 1);
+        }
     }
 
     #[test]
-    fn test_dims() {
-
-    }
-
-    #[test]
-    fn test_get() {
-
+    fn test_zeros() {        
+        let tensor: Tensor<i32> = Tensor::zeros(vec![2, 2]);
+        assert_eq!(tensor.shape, vec![2, 2]);
+        assert_eq!(tensor.strides, vec![2, 1]);
+        for idx in 0..tensor.row_major_length {
+            assert_eq!(tensor.data[idx], 0);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 mod base;
 
 fn main() {
-    let v = vec![[1, 2, 3], [4, 5, 6]];
+    let v: Vec<[i32; 3]> = vec![[1, 2, 3], [4, 5, 6]];
     let s = vec![2, 3];
-    let mut a = base::Tensor::new(v, s);
-    println!("Index out of bounds");
 }
