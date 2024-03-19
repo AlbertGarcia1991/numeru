1bbb5936500b3cb503730b65c8ddbfd4bda6330a
Albert Garcia Plaza <gplaza91@gmail.com>
2024-03-19 17:06:57 +0000

    Arithmetic operators overrided

--- src/base.rs
+++ src/base.rs
@@ -1,3 +1,5 @@
+use std::ops;
+
 use rand::Rng;
 
 #[derive(Debug, Clone)]
@@ -128,7 +130,39 @@ impl Tensor
         Tensor{ data, shape, strides, row_major_length }
     }
 
-    fn compute_strides(shape: &Vec<usize>) -> Vec<usize> {
+    fn get(&self, idx: &[usize]) -> f32 {
+        let length_idx: usize = idx.len();
+        if self.shape.len() != length_idx {
+            panic!("Cannot access to the request index, no matching dimensions");
+        }
+        let flat_idx: usize = idx.iter().zip(self.strides.iter()).map(|(x, y)| x * y).sum();
+        self.data[flat_idx]
+    }
+    
+    fn len(self) -> usize {
+        self.shape[0]
+    }
+
+    fn new_transpose(self) -> Self {
+        let mut new_shape: Vec<usize> = self.shape.clone();
+        new_shape.reverse();
+        Tensor::new(self.data, new_shape)
+    }
+    
+    fn transpose(&mut self) {
+        self.shape.reverse();
+        self.strides = Self::compute_strides(&self.shape);
+    }
+    
+    fn reshape(mut self, new_shape: Vec<usize>) {
+        let target_row_major_length: usize = new_shape.iter().product();
+        if self.row_major_length != target_row_major_length {
+            panic!("Cannot reshape the Tensor, shapes not compatible");
+        }
+        self.strides = new_shape;
+    }
+
+    pub fn compute_strides(shape: &Vec<usize>) -> Vec<usize> {
         let mut strides: Vec<usize> = Vec::with_capacity(shape.len());
         let mut stride: usize = 1;
         for &dimension in shape.iter().rev() {
@@ -138,54 +172,211 @@ impl Tensor
         strides.reverse();
         strides
     }
-
-    fn get(&self, idx: &[usize]) -> f32 {
-        let length_idx: usize = idx.len();
-        if self.shape.len() != length_idx {
-            panic!("Cannot access to the request index, no matching dimensions");
-        }
-        let flat_idx: usize = idx.iter().zip(self.strides.iter()).map(|(x, y)| x * y).sum();
-        self.data[flat_idx]
-    }
-
-    fn len(self) -> usize {
-        self.shape[0]
-    }
-
-    fn transpose(&self) -> Self {
-        let mut trans_tensor: Tensor = self.clone();
-        trans_tensor.shape = trans_tensor.shape.into_iter().rev().collect();
-        trans_tensor.strides = trans_tensor.strides.into_iter().rev().collect();
-        trans_tensor
-    }
-
-    fn reshape(mut self, new_shape: Vec<usize>) {
-        let target_row_major_length: usize = new_shape.iter().product();
-        if self.row_major_length != target_row_major_length {
-            panic!("Cannot reshape the Tensor, shapes not compatible");
-        }
-        self.strides = new_shape;
-    }
-
-    // OVERLOADING
-    // TODO: Pretty-printing
-    // TODO: Aritmetic operations
-    // TODO: Comparisons
+    
 }
 
-// TODO: Modulus and other metrics as a trait
+// TODO: Comparisons
+
+impl std::fmt::Display for Tensor {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?} (Shape: {:?})", self.data, self.shape)
+    }
+}
+
+impl ops::Add<Tensor> for Tensor {
+    type Output = Self;
+    fn add(self, _rhs: Tensor) -> Self::Output {
+        if self.shape != _rhs.shape {
+            panic!("Shapes do not match for addition");
+        }
+        let data: Vec<f32> = self.data.iter().zip(_rhs.data.iter()).map(|(a, b)| a + b).collect();
+        Tensor::new(data, self.shape)
+    }
+}
+
+impl ops::Add<f32> for Tensor {
+    type Output = Self;
+    fn add(self, _rhs: f32) -> Self::Output {
+        let data: Vec<f32> = self.data.iter().map(|&x| x + _rhs).collect();
+        Tensor::new(data, self.shape)
+    }
+}
+
+impl ops::AddAssign<Tensor> for Tensor {
+    fn add_assign(&mut self, _rhs: Self) {
+        if self.shape != _rhs.shape {
+            panic!("Shapes do not match for addition");
+        }
+        for (a, b) in self.data.iter_mut().zip(_rhs.data.iter()) {
+            *a += b;
+        }
+    }
+}
+
+impl ops::AddAssign<f32> for Tensor {
+    fn add_assign(&mut self, _rhs: f32) {
+        for a in self.data.iter_mut() {
+            *a += _rhs;
+        }
+    }
+}
+
+impl ops::Sub<Tensor> for Tensor {
+    type Output = Self;
+    fn sub(self, _rhs: Tensor) -> Self::Output {
+        if self.shape != _rhs.shape {
+            panic!("Shapes do not match for subtraction");
+        }
+        let data: Vec<f32> = self.data.iter().zip(_rhs.data.iter()).map(|(a, b)| a - b).collect();
+        Tensor::new(data, self.shape)
+    }
+}
+
+impl ops::Sub<f32> for Tensor {
+    type Output = Self;
+    fn sub(self, _rhs: f32) -> Self::Output {
+        let data = self.data.iter().map(|&x| x - _rhs).collect();
+        Tensor::new(data, self.shape)
+    }
+}
+
+impl ops::SubAssign<Tensor> for Tensor {
+    fn sub_assign(&mut self, _rhs: Self) {
+        if self.shape != _rhs.shape {
+            panic!("Shapes do not match for subtraction");
+        }
+        for (a, b) in self.data.iter_mut().zip(_rhs.data.iter()) {
+            *a -= b;
+        }
+    }
+}
+
+impl ops::SubAssign<f32> for Tensor {
+    fn sub_assign(&mut self, _rhs: f32) {
+        for a in self.data.iter_mut() {
+            *a -= _rhs;
+        }
+    }
+}
+
+impl ops::Mul<Tensor> for Tensor {
+    type Output = Self;
+    fn mul(self, _rhs: Tensor) -> Self::Output {
+        if self.shape != _rhs.shape {
+            panic!("Shapes do not match for multiplication");
+        }
+        let data: Vec<f32> = self.data.iter().zip(_rhs.data.iter()).map(|(a, b)| a * b).collect();
+        Tensor::new(data, self.shape)
+    }
+}
+
+impl ops::Mul<f32> for Tensor {
+    type Output = Self;
+    fn mul(self, _rhs: f32) -> Self::Output {
+        let data = self.data.iter().map(|&x| x * _rhs).collect();
+        Tensor::new(data, self.shape)
+    }
+}
+
+impl ops::MulAssign<Tensor> for Tensor {
+    fn mul_assign(&mut self, _rhs: Self) {
+        if self.shape != _rhs.shape {
+            panic!("Shapes do not match for multiplication");
+        }
+        for (a, b) in self.data.iter_mut().zip(_rhs.data.iter()) {
+            *a *= b;
+        }
+    }
+}
+
+impl ops::MulAssign<f32> for Tensor {
+    fn mul_assign(&mut self, _rhs: f32) {
+        for a in self.data.iter_mut() {
+            *a *= _rhs;
+        }
+    }
+}
+
+impl ops::Div<Tensor> for Tensor {
+    type Output = Self;
+    fn div(self, _rhs: Tensor) -> Self::Output {
+        if self.shape != _rhs.shape {
+            panic!("Shapes do not match for multiplication");
+        }
+        let data: Vec<f32> = self.data.iter().zip(_rhs.data.iter()).map(|(a, b)| a / b).collect();
+        Tensor::new(data, self.shape)
+    }
+}
+
+impl ops::Div<f32> for Tensor {
+    type Output = Self;
+    fn div(self, _rhs: f32) -> Self::Output {
+        let data = self.data.iter().map(|&x| x / _rhs).collect();
+        Tensor::new(data, self.shape)
+    }
+}
+
+impl ops::DivAssign<Tensor> for Tensor {
+    fn div_assign(&mut self, _rhs: Self) {
+        if self.shape != _rhs.shape {
+            panic!("Shapes do not match for multiplication");
+        }
+        for (a, b) in self.data.iter_mut().zip(_rhs.data.iter()) {
+            *a /= b;
+        }
+    }
+}
+
+impl ops::DivAssign<f32> for Tensor {
+    fn div_assign(&mut self, _rhs: f32) {
+        for a in self.data.iter_mut() {
+            *a /= _rhs;
+        }
+    }
+}
+
+impl ops::BitXor<f32> for Tensor {
+    type Output = Self;
+    fn bitxor(self, _rhs: f32) -> Self::Output {
+        let data: Vec<f32> = self.data.iter().map(|&x| x.powf(_rhs)).collect();
+        Tensor::new(data, self.shape)
+    }
+}
+
+impl ops::BitXorAssign<f32> for Tensor {
+    fn bitxor_assign(&mut self, _rhs: f32) {
+        for a in self.data.iter_mut() {
+            *a = a.powf(_rhs);
+        }
+    }
+}
+
+impl ops::Neg for Tensor {
+    type Output = Self;
+    fn neg(self) -> Self::Output {
+        let data: Vec<f32> = self.data.iter().map(|&x| -x).collect();
+        Tensor::new(data, self.shape)
+    }
+}
+
+// TODO: Override ^ between tensors as dot product
+// TODO: Override % between tensors as scalar product
+// TODO: Override & between tensors as angle for each dimension
+// TODO: Implement fn modulus (how to handle dimensions ?)
+// TODO: Override ! unary tensors as normalization (how to handle dimensions ?)
+// TODO: Replace panic! by Result<Self, Error>
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    
     fn get_dummy_tensor_from_new() -> Tensor {
         let data: Vec<f32> = vec![1., 2., 3., 4., 5., 6.];
         let shape: Vec<usize> = vec![2, 3];
         let tensor: Tensor = Tensor::new(data, shape);
         tensor
     }
-
+    
     #[test]
     fn test_new_attributes() {
         let tensor: Tensor = get_dummy_tensor_from_new();
@@ -254,7 +445,6 @@ mod tests {
 
     #[test]
     fn test_new_ones_like() {
-        let tensor_dummy: Tensor = get_dummy_tensor_from_new();
         let tensor: Tensor = Tensor::ones_like(&Tensor::zeros(vec![10]));
         assert_eq!(tensor.data, vec![1.; 10]);
         let tensor: Tensor = Tensor::ones_like(&Tensor::zeros(vec![6]));
@@ -298,9 +488,60 @@ mod tests {
 
     #[test]
     fn test_transpose() {
+        let mut tensor: Tensor = get_dummy_tensor_from_new();
+        tensor.transpose();
+        assert_eq!(tensor.shape, vec![3, 2]);
+        assert_eq!(tensor.strides, vec![2, 1]);
+    }
+
+    #[test]
+    fn test_new_transpose() {
         let tensor: Tensor = get_dummy_tensor_from_new();
-        let trans_tensor: Tensor = tensor.transpose();
-        assert_eq!(trans_tensor.shape, vec![3, 2]);
-        assert_eq!(trans_tensor.strides, vec![3, 2]);
+        let reversed_tensor: Tensor = tensor.new_transpose();
+        assert_eq!(reversed_tensor.shape, vec![3, 2]);
+        assert_eq!(reversed_tensor.strides, vec![2, 1]);
+    }
+
+    #[test]
+    fn test_neg() {
+        let tensor1: Tensor = -get_dummy_tensor_from_new();
+        assert_eq!(tensor1.data, vec![-1., -2., -3., -4., -5., -6.]);
+    }
+
+    #[test]
+    fn test_add_tensor() {
+        let tensor1: Tensor = get_dummy_tensor_from_new();
+        let tensor2: Tensor = get_dummy_tensor_from_new();
+        let tensor3: Tensor = tensor1 + tensor2;
+        assert_eq!(tensor3.data, vec![2., 4., 6., 8., 10., 12.]);
+    }
+
+    #[test]
+    fn test_add_scalar() {
+        let tensor1: Tensor = get_dummy_tensor_from_new();
+        let tensor2: Tensor = tensor1 + 1.;
+        assert_eq!(tensor2.data, vec![2., 3., 4., 5., 6., 7.]);
+    }
+
+    #[test]
+    fn test_add_tensor_inplace() {
+        let mut tensor1: Tensor = get_dummy_tensor_from_new();
+        let tensor2: Tensor = get_dummy_tensor_from_new();
+        tensor1 += tensor2;
+        assert_eq!(tensor1.data, vec![2., 4., 6., 8., 10., 12.]);
+    }
+
+    #[test]
+    fn test_add_scalar_inplace() {
+        let mut tensor1: Tensor = get_dummy_tensor_from_new();
+        tensor1 += 1.;
+        assert_eq!(tensor1.data, vec![2., 3., 4., 5., 6., 7.]);
+    }
+
+    #[test]
+    fn test_print() {
+        // TODO: Add strides and shape to print as N-Dimensional array
+        let tensor: Tensor = get_dummy_tensor_from_new();
+        println!("{}", tensor);
     }
 }
