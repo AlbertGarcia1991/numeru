d26f0c57c62f1d7c5929601fb3604c0bae4747f5
AlbertGarciaPlaza <albert.plaza@lego.com>
2024-12-16 14:09:51 +0000

    Refactor:Project setup:Codebase Run cargo clippy Run cargo fmt
    JIRA: NUM-8 -> DONE

--- src/bin/executable.rs
+++ src/bin/executable.rs
@@ -3,6 +3,6 @@ extern crate numeru;
 use numeru::data_structures::ds_array::array_static::ArrayStatic;
 
 fn main() {
-    let a: ArrayStatic = ArrayStatic {capacity: 10};
+    let a: ArrayStatic = ArrayStatic { capacity: 10 };
     println!("{}", a);
 }
