30a8c55b5829a5da876249b4ab223db2f5a5d6d4
AlbertGarciaPlaza <albert.plaza@lego.com>
2024-12-23 10:15:57 +0100

    Feat:Static Array:Getters Add get_at method and its test Add test for private methods: check_shape_with_capacity, check_index_within_bounds, and calculate_flat_index Refactor methods to correctly take self structure as parameter
    JIRA: NUM-16

--- src/data_structures/ds_array/static_array.rs
+++ src/data_structures/ds_array/static_array.rs
@@ -19,7 +19,7 @@ impl StaticArray {
         }
     }
 
-    fn _check_shape_capacity_match(_capacity: &usize, _shape: &[usize]) {
+    fn _check_shape_capacity_match(_capacity: &usize, _shape: &[usize]) -> bool{
         let shape_product: usize = _shape.iter().product();
         if shape_product != *_capacity {
             panic!(
@@ -27,6 +27,29 @@ impl StaticArray {
                 shape_product, *_capacity
             );
         }
+        true
+    }
+
+    fn _check_index_within_bounds(&self, indices: &[usize]) -> bool {
+        if indices.len() != self.shape.len() {
+            panic!("Number of indices does not match the shape dimensions");
+        }
+        for (i, &index) in indices.iter().enumerate() {
+            if index >= self.shape[i] {
+                panic!("Index out of bounds for dimension {}: {} >= {}", i, index, self.shape[i]);
+            }
+        }
+        true
+    }
+
+    fn _calculate_flat_index(&self, indices: &[usize]) -> usize {
+        let mut flat_index = 0;
+        let mut stride = 1;
+        for (i, &index) in indices.iter().rev().enumerate() {
+            flat_index += index * stride;
+            stride *= self.shape[self.shape.len() - 1 - i];
+        }
+        flat_index
     }
 
     pub fn new_zeros(shape: Vec<usize>) -> Self {
@@ -52,8 +75,10 @@ impl StaticArray {
         }
     }
 
-    pub fn get_at() -> f32 {
-        3.14
+    pub fn get_at(&self, indices: &[usize]) -> f32 {
+        self._check_index_within_bounds(indices);
+        let flat_index: usize = self._calculate_flat_index(indices);
+        self.data[flat_index]
     }
 
     // pub fn get_slice() -> StaticArray {
@@ -74,7 +99,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn create_static_array_without_constructor() {
+    fn test_create_static_array_without_constructor() {
         let static_array: StaticArray = StaticArray {
             capacity: 1,
             data: Vec::from([10.]),
@@ -85,7 +110,7 @@ mod tests {
     }
 
     #[test]
-    fn create_static_array_with_zeros() {
+    fn test_create_static_array_with_zeros() {
         let ref_array: StaticArray = StaticArray {
             capacity: 2,
             data: Vec::from([0., 0.]),
@@ -98,7 +123,7 @@ mod tests {
     }
 
     #[test]
-    fn create_static_array_with_ones() {
+    fn test_create_static_array_with_ones() {
         let ref_array: StaticArray = StaticArray {
             capacity: 4,
             data: Vec::from([1., 1., 1., 1.]),
@@ -111,7 +136,7 @@ mod tests {
     }
 
     #[test]
-    fn create_static_array_with_fill() {
+    fn test_create_static_array_with_fill() {
         let ref_array: StaticArray = StaticArray {
             capacity: 2,
             data: Vec::from([3.14, 3.14]),
@@ -124,7 +149,7 @@ mod tests {
     }
 
     #[test]
-    fn create_static_array_from_array() {
+    fn test_create_static_array_from_array() {
         let ref_array: StaticArray = StaticArray {
             capacity: 6,
             data: Vec::from([1., 2., 3., 4., 5., 6.]),
@@ -149,18 +174,57 @@ mod tests {
         assert_eq!(ref_array.shape, array.shape);
     }
 
-    // #[test]
-    // fn get_static_array_element() {
-    //     let array: StaticArray = StaticArray {
-    //         capacity: 6,
-    //         data: Vec::from([1., 2., 3., 4., 5., 6.]),
-    //         shape: Vec::from([2, 3]),
-    //     };
-    //     assert_eq!(array.get_at([0, 0]), 1.);
-    //     assert_eq!(array.get_at([0, 1]), 2.);
-    //     assert_eq!(array.get_at([0, 2]), 3.);
-    //     assert_eq!(array.get_at([1, 0]), 4.);
-    //     assert_eq!(array.get_at([1, 1]), 5.);
-    //     assert_eq!(array.get_at([1, 2]), 6.);
-    // }
+    #[test]
+    #[should_panic]
+    fn test_check_shape_capacity_match() {
+        let mut capacity: usize = 6;
+        let shape: Vec<usize> = Vec::from([2, 3]);
+        assert!(StaticArray::_check_shape_capacity_match(&capacity, &shape));
+        capacity = 5;
+        StaticArray::_check_shape_capacity_match(&capacity, &shape);
+    }
+    
+    #[test]
+    #[should_panic]
+    fn test_check_index_within_bounds() {
+        let array: StaticArray = StaticArray {
+            capacity: 6,
+            data: Vec::from([1., 2., 3., 4., 5., 6.]),
+            shape: Vec::from([2, 3]),
+        };
+        let indices: Vec<usize> = Vec::from([2, 1]);
+        assert!(array._check_index_within_bounds(&indices));
+        let indices: Vec<usize> = Vec::from([1, 3]);
+        array._check_index_within_bounds(&indices);
+    }
+
+    #[test]
+    fn test_calculate_flat_index() {
+        let array: StaticArray = StaticArray {
+            capacity: 6,
+            data: Vec::from([1., 2., 3., 4., 5., 6.]),
+            shape: Vec::from([2, 3]),
+        };
+        assert_eq!(array._calculate_flat_index(&Vec::from([0, 0])), 0);
+        assert_eq!(array._calculate_flat_index(&Vec::from([0, 1])), 1);
+        assert_eq!(array._calculate_flat_index(&Vec::from([0, 2])), 2);
+        assert_eq!(array._calculate_flat_index(&Vec::from([1, 0])), 3);
+        assert_eq!(array._calculate_flat_index(&Vec::from([1, 1])), 4);
+        assert_eq!(array._calculate_flat_index(&Vec::from([1, 2])), 5);
+    }   
+
+    #[test]
+    fn test_get_static_array_element() {
+        let array: StaticArray = StaticArray {
+            capacity: 6,
+            data: Vec::from([1., 2., 3., 4., 5., 6.]),
+            shape: Vec::from([2, 3]),
+        };
+        assert_eq!(array.get_at(&[0, 0]), 1.);
+        assert_eq!(array.get_at(&[0, 1]), 2.);
+        assert_eq!(array.get_at(&[0, 2]), 3.);
+        assert_eq!(array.get_at(&[1, 0]), 4.);
+        assert_eq!(array.get_at(&[1, 1]), 5.);
+        assert_eq!(array.get_at(&[1, 2]), 6.);
+    }
 }
